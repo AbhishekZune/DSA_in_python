119a5947f5ef23cc3797e957a503a85766012fb0
AbhishekZune <91654492+AbhishekZune@users.noreply.github.com>
2022-09-06 04:12:09 +0000

    brute force p^2 and optimize O(n) way to compare two lists

---
+++ same_ele_in_2_arr.py
@@ -0,0 +1,19 @@
+# arr1 = list(map(int, input().split()))
+# arr2 = list(map(int, input().split()))
+# for i in arr1:
+#   for j in arr2:
+#     if i == j:
+#         print(True)
+# print(False)
+
+# # from os import system
+# # system("python same_ele_in_2_arr.py") system("python same_ele_in_2_arr.py")
+
+
+arr1 = list(map(str,input().split()))
+arr2 = list(map(str,input().split()))
+d1 = {x : arr1.count(x) for x in arr1}
+for i in arr2:
+  if i in d1.keys():
+    print(True)
+print(False)
